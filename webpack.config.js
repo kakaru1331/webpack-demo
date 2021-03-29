8b41fbcb5c318427693745aa6f72a6316438fdcd
JeKim <kakaru@einz.co.kr>
2021-03-30 08:08:33 +0900

    build: refactor output management strategy

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
       {
@@ -29,11 +24,15 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html'
-    }),
-    new CleanWebpackPlugin()
+    })
   ],
   devServer: {
     contentBase: './dist',
     open: true
-  }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
 }
